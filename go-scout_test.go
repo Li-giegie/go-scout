7cf4ec529ebef71a5d9f8531f34a6b50d19ee23d
li jinlong <1261930106@qq.com>
2023-01-13 15:49:32 +0800

    服务版合并更新

--- go-scout_test.go
+++ go-scout_test.go
@@ -1,27 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"testing"
-)
-
-func TestNew(t *testing.T) {
-
-	scout,Paths,err := New(1000,"./")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println("Paths：",Paths)
-
-	scout.SetDebug()
-	err = scout.Scout(func(changePath *[]ScoutChange) {
-		for _, change := range *changePath {
-			fmt.Printf("type:%v path:%v \n",change.Type,change.Path)
-		}
-	})
-
-	fmt.Println(err)
-}
+//func TestNew(t *testing.T) {
+//
+//	scout,Paths,err := New(1000,"./")
+//	if err != nil {
+//		log.Fatalln(err)
+//	}
+//	fmt.Println("Paths：",Paths)
+//
+//	scout.SetDebug()
+//	err = scout.Scout(func(changePath *[]ScoutChange) {
+//		for _, change := range *changePath {
+//			fmt.Printf("type:%v path:%v \n",change.Type,change.Path)
+//		}
+//	})
+//
+//	fmt.Println(err)
+//}
 
 
